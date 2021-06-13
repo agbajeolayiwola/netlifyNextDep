997dce87c6d374ced0a0228f51f2d796da3d38aa
Olayiwola <agbajeolaiwola@gmail.com>
2021-06-13 18:06:56 +0100

    ''

--- next.config.js
+++ next.config.js
@@ -2,17 +2,8 @@ const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   pwa: {
-    dest: "public",
+    dest: "/",
     register: true,
     skipWaiting: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/pages/index.js',
-        destination: '/',
-        permanent: true,
-      },
-    ]
   },
 });
